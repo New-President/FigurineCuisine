e9810c391d2f4f39c48e02c72bfa996690c710d5
Long_Teck <longteck@gmail.com>
2022-07-12 23:30:04 +0800

    Input Validation

--- FigurineCuisine/Models/Figurine.cs
+++ FigurineCuisine/Models/Figurine.cs
@@ -8,14 +8,21 @@ namespace FigurineCuisine.Models
     public class Figurine
     {
         public int ID { get; set; }
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter valid string.")]
         public string Name { get; set; }
         public int RetailerID { get; set; }
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter valid string.")]
         public string Brand { get; set; }
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter valid string.")]
         public string Manufacturer { get; set; }
 
         [Display(Name = "Publish Date")]
         [DataType(DataType.Date)]
         public DateTime PublishedDate { get; set; }
+
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public int Ratings { get; set; }
