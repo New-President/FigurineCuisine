204fa9c6c6a6647d262a41b4a2427f2e3d4b5ca6
Long_Teck <longteck@gmail.com>
2022-07-14 13:43:30 +0800

    Customers Created

---
+++ FigurineCuisine/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FigurineCuisine.Data;
+using FigurineCuisine.Models;
+
+namespace FigurineCuisine.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FigurineCuisine.Data.FigurineCuisineContext _context;
+
+        public IndexModel(FigurineCuisine.Data.FigurineCuisineContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customer> Customer { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Customer = await _context.Customers.ToListAsync();
+        }
+    }
+}
