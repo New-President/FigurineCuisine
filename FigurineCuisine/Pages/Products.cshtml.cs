2c116db752eedc5d7b40988f161ad51c0786159b
Wee Kang Lee <s10222162@connect.np.edu.sg>
2022-07-25 18:02:27 +0800

    filter

--- FigurineCuisine/Pages/Products.cshtml.cs
+++ FigurineCuisine/Pages/Products.cshtml.cs
@@ -7,11 +7,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FigurineCuisine.Data;
 using FigurineCuisine.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FigurineCuisine.Pages
 {
     public class ProductsModel : PageModel
     {
+
+        public IList<Figurine> Figurine { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList ? Category { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? FigurineCategory { get; set; }
+
         private readonly FigurineCuisine.Data.FigurineCuisineContext _context;
 
         public ProductsModel(FigurineCuisine.Data.FigurineCuisineContext context)
@@ -23,7 +33,23 @@ namespace FigurineCuisine.Pages
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> categoryQuery = from m in _context.Figurine
+                                               orderby m.Category
+                                               select m.Category;
             ApplicationRole = await _context.Roles.ToListAsync();
+
+            var figurines = from m in _context.Figurine
+                         select m; 
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                figurines = figurines.Where(s => s.Name.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(FigurineCategory))
+            {
+                figurines = figurines.Where(x => x.Category == FigurineCategory);
+            }
+            Category = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            Figurine = await figurines.ToListAsync();
         }
     }
 }
