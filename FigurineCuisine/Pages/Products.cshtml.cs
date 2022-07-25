369a50c1b2836ea67ae63ec38b384cdbdf202d12
Yong Zi Ren <s10219524@connect.np.edu.sg>
2022-07-25 17:01:23 +0800

    Added products

---
+++ FigurineCuisine/Pages/Products.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FigurineCuisine.Data;
+using FigurineCuisine.Models;
+
+namespace FigurineCuisine.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly FigurineCuisine.Data.FigurineCuisineContext _context;
+
+        public ProductsModel(FigurineCuisine.Data.FigurineCuisineContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ApplicationRole> ApplicationRole { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            ApplicationRole = await _context.Roles.ToListAsync();
+        }
+    }
+}
