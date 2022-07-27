6a71c9c501ccf038aac397d4aecd3f2c50cac171
Wee Kang Lee <s10222162@connect.np.edu.sg>
2022-07-27 23:16:30 +0800

    not working cart

--- FigurineCuisine/Pages/ProductView.cshtml.cs
+++ FigurineCuisine/Pages/ProductView.cshtml.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FigurineCuisine.Pages
 {
     public class ProductViewModel : PageModel
     {
+        [BindProperty]
         public Figurine selectedProduct { get; set; }
         private readonly FigurineCuisine.Data.FigurineCuisineContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
@@ -21,6 +23,37 @@ namespace FigurineCuisine.Pages
         public async Task OnGetAsync(int id)
         {
             selectedProduct = await _context.Figurine.FindAsync(id);
+        }
+        public ProductInput Input { get; set; }
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            // GetCartByUserIdAsync
+            var cart = await _context.Cart.FirstOrDefaultAsync(cart => cart.UserID == user.Id);
+            if (ModelState.IsValid)
+            {
+                CartItems cartItem = new CartItems
+                {
+                    CartID = cart.ID,
+                    ProductID = id,
+                    Quantity = Input.Quantity
+                };
+                if (await _shop.GetCartItemByProductIdForUserAsync(user.Id, id) != null)
+                {
+                    CartItems existingCartItem = await _shop.GetCartItemByProductIdForUserAsync(user.Id, id);
+                    existingCartItem.Quantity += Input.Quantity;
+                    await _shop.UpdateCartItemsAsync(existingCartItem);
+                }
+                else
+                {
+                    await _shop.CreateCartItemAsync(cartItem);
+                }
+            }
+            return Redirect("/Shop/Cart");
+        }
+        public class ProductInput
+        {
+            public int Quantity { get; set; } = 1;
         }
     }
 }
