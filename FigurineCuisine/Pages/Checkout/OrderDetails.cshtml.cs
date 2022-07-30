a350eb294646c74f7fa8d0a06865ac55d8bc41e1
Wee Kang Lee <s10222162@connect.np.edu.sg>
2022-07-30 18:05:05 +0800

    order authentication

--- FigurineCuisine/Pages/Checkout/OrderDetails.cshtml.cs
+++ FigurineCuisine/Pages/Checkout/OrderDetails.cshtml.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using AuthorizeNet.Api.Contracts.V1;
+using FigurineCuisine.Areas.Identity.Pages.Account;
 using FigurineCuisine.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FigurineCuisine.Pages.Checkout
 {
@@ -20,10 +22,12 @@ namespace FigurineCuisine.Pages.Checkout
     public class OrderDetailsModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
         private readonly Data.FigurineCuisineContext _context;
+        private readonly ILogger<LoginWith2faModel> _logger;
 
-
+        public string ReturnUrl { get; set; }
         //private readonly IShop _shop;
         //private readonly IPayment _paymnet;
         //private readonly IOrder _order;
@@ -36,11 +40,13 @@ namespace FigurineCuisine.Pages.Checkout
         /// <param name="shop"></param>
         /// <param name="payment"></param>
         /// <param name="order"></param>
-        public OrderDetailsModel(UserManager<ApplicationUser> userManager, IEmailSender emailSender, Data.FigurineCuisineContext context)
+        public OrderDetailsModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailSender emailSender, Data.FigurineCuisineContext context, ILogger<LoginWith2faModel> logger)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
             _emailSender = emailSender;
             _context = context;
+            _logger = logger;
         }
 
         /// <summary>
@@ -52,10 +58,6 @@ namespace FigurineCuisine.Pages.Checkout
         public async Task<Cart> GetCartByUserIdAsync(string userId) => await _context.Cart.FirstOrDefaultAsync(cart => cart.UserID == userId);
 
 
-        public void OnGet()
-        {
-        }
-
         /// <summary>
         /// This post operation uses UserManager to get the current signed in user
         /// Set a variable to store the total costs of all items in the cart
@@ -63,18 +65,46 @@ namespace FigurineCuisine.Pages.Checkout
         /// After the email is sent out, redirect the user to receipt page
         /// </summary>
         /// <returns>If the ckeckout process is successful, redirect to the receipt page. Otherwise, returns to the same page</returns>
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(bool rememberMe, string returnUrl = null)
         {
             if (ModelState.IsValid)
             {
-                ApplicationUser user = await _userManager.GetUserAsync(User);
+                //ApplicationUser user = await _userManager.GetUserAsync(User);
                 //IEnumerable<CartItem> cartItems = await GetCartItemsByUserIdAsync(user.Id);
 
                 //await RemoveCartItemsAsync(cartItems);
 
                 return Redirect("/Checkout/Receipt");
              }
-   
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication user.");
+            }
+
+            var authenticatorCode = Input.TwoFactorCode.Replace(" ", string.Empty).Replace("-", string.Empty);
+
+            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, false, false);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User with ID '{UserId}' logged in with 2fa.", user.Id);
+                return LocalRedirect(returnUrl);
+            }
+            else if (result.IsLockedOut)
+            {
+                _logger.LogWarning("User with ID '{UserId}' account locked out.", user.Id);
+                return RedirectToPage("./Lockout");
+            }
+            else
+            {
+                _logger.LogWarning("Invalid authenticator code entered for user with ID '{UserId}'.", user.Id);
+                ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+                return Page();
+            }
             return Page();
         }
         
@@ -104,6 +134,12 @@ namespace FigurineCuisine.Pages.Checkout
 
             [Required]
             public CreditCard CreditCard { get; set; }
+
+            [Required]
+            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Text)]
+            [Display(Name = "Authenticator code")]
+            public string TwoFactorCode { get; set; }
         }
 
         public enum CreditCard
